e5fdd617dca898694830d49717b7787baba4836b
Marie Bastiaens <r0891122@ucll.be>
2024-09-30 19:26:48 +0200

    Initialization of project

---
+++ f1diary-back-end/src/main/java/com/f1diary/f1diary_back_end/F1diaryBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.f1diary.f1diary_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1diaryBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1diaryBackEndApplication.class, args);
+	}
+
+}

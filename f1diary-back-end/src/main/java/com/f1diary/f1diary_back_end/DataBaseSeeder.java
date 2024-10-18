157070cb21f6a9a103bad16668a12e64e88a78b4
bastiaensMarie <bastiaensma@gmail.com>
2024-10-18 21:40:41 +0200

    Database seeder for easy testing made

--- f1diary-back-end/src/main/java/com/f1diary/f1diary_back_end/DataBaseSeeder.java
+++ f1diary-back-end/src/main/java/com/f1diary/f1diary_back_end/DataBaseSeeder.java
@@ -2,6 +2,7 @@ package com.f1diary.f1diary_back_end;
 
 import com.f1diary.f1diary_back_end.Race.Model.Race;
 import com.f1diary.f1diary_back_end.Race.Repository.RaceRepository;
+import com.f1diary.f1diary_back_end.RaceResult.Model.RaceResult;
 import com.f1diary.f1diary_back_end.RaceResult.Repository.RaceResultRepository;
 import com.f1diary.f1diary_back_end.Racer.Model.Racer;
 import com.f1diary.f1diary_back_end.Racer.Repository.RacerRepository;
@@ -17,28 +18,37 @@ public class DataBaseSeeder {
     @Bean
     CommandLineRunner initDataBase(TeamRepository teamRepository, RaceRepository raceRepository, RacerRepository racerRepository, RaceResultRepository raceResultRepository) {
         return args -> {
+            Team team = new Team();
             if (teamRepository.findTeamByTeamName("testTeam").isEmpty()) {
-                Team team = new Team();
                 team.setTeamName("testTeam");
                 team.setTotalPoints(0);
                 teamRepository.save(team);
             }
 
+            Race race = new Race();
             if (raceRepository.findRaceByRaceName("testRace").isEmpty()) {
-                Race race = new Race();
                 race.setRaceName("testRace");
                 race.setLocation("test");
                 race.setYear(2000);
                 raceRepository.save(race);
             }
 
+            Racer racer = new Racer();
             if (racerRepository.findRacerByLastName("test").isEmpty()) {
-                Racer racer = new Racer();
                 racer.setFirstName("test");
                 racer.setLastName("test");
                 racer.setTotalPoints(0);
+                racer.setTeam(team);
                 racerRepository.save(racer);
             }
+
+            RaceResult raceResult = new RaceResult();
+            raceResult.setRaceId(race);
+            raceResult.setTeamId(team);
+            raceResult.setRacerId(racer);
+            raceResult.setPosition(1);
+            raceResult.setPoints(25);
+            raceResultRepository.save(raceResult);
 
         };
     }
